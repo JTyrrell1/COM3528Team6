2c901c855f4e0d4ebf348f16be172ec9a67b3548
Sophie <jtyrrell1@sheffield.ac.uk>
2025-04-30 13:16:19 +0100

    set up the vapi call start

--- Team6_work/therapy/vapi_therapist.py
+++ Team6_work/therapy/vapi_therapist.py
@@ -1,3 +1,6 @@
-from vapi_python.vapi_python import Vapi
+from vapi_python import Vapi
 
-vapi = Vapi(api_key='')
+vapi = Vapi(api_key='c524cc97-ece6-4fcd-a045-ddfec239fcb6')
+vapi.start(assistant_id='7e34bfbd-5fdd-4aef-9a3b-17b3e29f9bf4')
+
+
